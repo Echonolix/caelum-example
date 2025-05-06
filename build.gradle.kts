3315420e1dfd259100343cf2580263287a3ed0e9
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-05-06 01:40:10 -0400

    No more lwjgl

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,16 +13,7 @@ plugins {
     id("net.echonolix.slang-gradle-plugin")
 }
 
-val lwjglVersion = "3.3.6"
-val lwjglNatives = "natives-windows"
-
 dependencies {
-    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
-    implementation("org.lwjgl", "lwjgl", lwjglVersion)
-    implementation("org.lwjgl", "lwjgl-shaderc", lwjglVersion)
-    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
-    runtimeOnly("org.lwjgl", "lwjgl-shaderc", classifier = lwjglNatives)
-
     implementation("net.echonolix:caelum-core:1.0-SNAPSHOT")
     implementation("net.echonolix:caelum-vulkan:1.0-SNAPSHOT")
     implementation("net.echonolix:caelum-glfw-vulkan:1.0-SNAPSHOT")
